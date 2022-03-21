63ff94884c83186ceb9cc83185eaa66307ad805b
Martí Bosch <marti.bosch@protonmail.com>
2022-03-21 19:19:10 +0100

    fix: missing default plot settings

--- agrometeo/settings.py
+++ agrometeo/settings.py
@@ -1,3 +1,14 @@
 """Settings."""
+# core
 STATION_ID_NAME = "station_id"
 TIME_NAME = "time"
+
+# plotting
+PLOT_CMAP = "coolwarm"
+PLOT_LEGEND = True
+PLOT_ATTRIBUTION = False
+PLOT_LEGEND_POSITION = "right"
+PLOT_LEGEND_SIZE = "2.5%"
+PLOT_LEGEND_PAD = 0.2
+PLOT_TITLE = True
+PLOT_ADD_BASEMAP = True
--- tests/test_agrometeo.py
+++ tests/test_agrometeo.py
@@ -3,10 +3,12 @@
 # pylint: disable=redefined-outer-name
 from datetime import datetime, timedelta
 
+import numpy as np
+
 import agrometeo as agm
 
 
-def test_core():
+def test_agrometeo():
     agm_ds = agm.AgrometeoDataset(region="Pully, Switzerland")
     num_stations = len(agm_ds.station_gdf)
     assert num_stations >= 1
@@ -18,3 +20,22 @@ def test_core():
     ts_gdf = agm_ds.get_ts_gdf(start_date=start_date, end_date=end_date)
     assert len(ts_gdf) == num_stations
     assert ts_gdf["geometry"].isna().sum() == 0
+
+    # test plotting
+    # use `add_basemap=False` to avoid having to mock contextily's requests
+    ax = agm.plot_temperature_map(ts_gdf, add_basemap=False)
+    assert len(ax.get_title()) > 0
+    ax = agm.plot_temperature_map(ts_gdf, title=False, add_basemap=False)
+    assert len(ax.get_title()) == 0
+    assert len(ax.collections[0].get_array()) == num_stations
+    axes = [
+        agm.plot_temperature_map(
+            ts_gdf,
+            dt=dt,
+            add_basemap=False,
+        )
+        for dt in ts_gdf.columns.drop("geometry")[:2]
+    ]
+    assert not np.array_equal(
+        axes[0].collections[0].get_array(), axes[1].collections[0].get_array()
+    )
