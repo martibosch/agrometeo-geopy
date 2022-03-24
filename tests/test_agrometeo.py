b2fe32d2c4830536b536c25e1bacf96ff143697f
Martí Bosch <marti.bosch@epfl.ch>
2022-03-24 10:50:07 +0100

    updated plot test with fixed dates to ensure deterministic results

--- tests/test_agrometeo.py
+++ tests/test_agrometeo.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 """Tests for `agrometeo` package."""
 # pylint: disable=redefined-outer-name
-from datetime import datetime, timedelta
-
 import numpy as np
 
 import agrometeo as agm
@@ -12,9 +10,8 @@ def test_agrometeo():
     agm_ds = agm.AgrometeoDataset(region="Pully, Switzerland")
     num_stations = len(agm_ds.station_gdf)
     assert num_stations >= 1
-    today = datetime.today()
-    start_date = today - timedelta(days=2)
-    end_date = today - timedelta(days=1)
+    start_date = "2022-03-22"
+    end_date = "2022-03-23"
     ts_df = agm_ds.get_ts_df(start_date=start_date, end_date=end_date)
     assert len(ts_df.columns) == num_stations
     ts_gdf = agm_ds.get_ts_gdf(start_date=start_date, end_date=end_date)
@@ -28,13 +25,14 @@ def test_agrometeo():
     ax = agm.plot_temperature_map(ts_gdf, title=False, add_basemap=False)
     assert len(ax.get_title()) == 0
     assert len(ax.collections[0].get_array()) == num_stations
+    ts_columns = ts_gdf.columns.drop("geometry")
     axes = [
         agm.plot_temperature_map(
             ts_gdf,
             dt=dt,
             add_basemap=False,
         )
-        for dt in ts_gdf.columns.drop("geometry")[:2]
+        for dt in [ts_columns[0], ts_columns[-1]]
     ]
     assert not np.array_equal(
         axes[0].collections[0].get_array(), axes[1].collections[0].get_array()
